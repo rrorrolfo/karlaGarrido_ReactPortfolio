39923d7904b3103d15d072048b83ded1534f729b
Rodolfo Perez <rodolfobeste@gmail.com>
2019-03-12 11:11:18 +0200

    Changed alias in copywright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
     return (
         <div id="copywright">
-            <p>&copy; 2019 Karla Garrido | <span id="autor">Crafted by Rodolfo del Rio</span></p>
+            <p>&copy; 2019 Karla Garrido | <span id="autor">Crafted by rrorrolfo</span></p>
         </div>
     )
 }
