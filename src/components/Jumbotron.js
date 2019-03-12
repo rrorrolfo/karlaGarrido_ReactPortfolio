f5597c5f383b41af4f9686502d6a8be91d5ea541
Rodolfo Perez <rodolfobeste@gmail.com>
2019-03-12 11:15:25 +0200

    Added mobile and desktop main background images

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,10 +1,13 @@
 import React from "react";
-const portada = require("../img/portada.jpg");
+const portada_lg = require("../img/portada.jpg");
+const portada_sm = require("../img/portada_mob.jpg");
 
 const Jumbotron = () => {
     return (
         <div id="jumbotron">
-            <img src={ portada } alt="Actriz Mexicana Karla Garrido" title="Actriz Mexicana Karla Garrido"/>
+            <img src={ portada_sm } className="portada_sm" alt="Actriz Mexicana Karla Garrido" title="Actriz Mexicana Karla Garrido"/>
+            <img src={ portada_lg } className="portada_lg" alt="Actriz Mexicana Karla Garrido" title="Actriz Mexicana Karla Garrido"/>
+
             <div id="presentation_text">
                 <p>Karla Garrido</p>
                 <p>Actriz</p>
