2efd4237e1b0e7f8d8a6802d575db20df8fcbb8c
Rodolfo Perez <rodolfobeste@gmail.com>
2019-03-16 11:52:46 +0200

    Added sticky footer and changed order of h2

--- src/components/demos/Demos.js
+++ src/components/demos/Demos.js
@@ -3,19 +3,21 @@ import React from "react";
 const Demos = () => {
     return(
 
-        <div id="demos">
+        <div id="demos" className="sticky_footer">
 
             <div>
-                <h2>Demo Reel 2016</h2>
                 <iframe src="https://www.youtube.com/embed/2NQSHHJWxD0" title="Demo Reel 2016"></iframe>
+                <h2>Demo Reel 2016</h2>
             </div>
+
             <div>
-                <h2>Demo Reel 2014</h2>
                 <iframe src="https://www.youtube.com/embed/9wVIiiLDv34" title="Demo Reel 2014"></iframe>
+                <h2>Demo Reel 2014</h2>
             </div>
+            
             <div>
-                <h2>Demo Reel "Comedia"</h2>
                 <iframe src="https://www.youtube.com/embed/SxGLRXrPgaI" title="Demo Reel Comedia"></iframe>
+                <h2>Demo Reel "Comedia"</h2>
             </div>
 
         </div>
