0a3a3d29d1738f6548ceca82dcde15d2eee839ee
Rodolfo Perez <rodolfobeste@gmail.com>
2019-03-17 12:20:22 +0200

    Added and styled responsiveness for main navigation for mobile screens

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,14 +2,37 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Nav = () => {
+
+    const toogle_menu = () => {
+        const nav_menu = document.querySelector(".main_nav");
+        
+        nav_menu.className === "main_nav" ? (
+            nav_menu.classList.add("is_display")
+            ) : (
+            nav_menu.className = "main_nav"
+        )
+    }
+
     return (
-        <ul id="main_nav">
-            <li><NavLink to="/acerca">Acerca</NavLink></li>
-            <li><NavLink to="/portafolio">Portafolio</NavLink></li>
-            <li><NavLink to="/trayectoria">Trayectoria</NavLink></li>
-            <li><NavLink to="/demos">Demo Reel</NavLink></li>
-            <li><NavLink to="/contacto">Contacto</NavLink></li>
-        </ul>
+        <React.Fragment>
+
+            <div onClick={ toogle_menu }>
+                <svg xmlns="http://www.w3.org/2000/svg" id="menu_bars" viewBox="0 0 50 50" version="1.1" width="30px" height="40px" >
+                    <g id="surface1">
+                    <path d="M 0 7.5 L 0 12.5 L 50 12.5 L 50 7.5 Z M 0 22.5 L 0 27.5 L 50 27.5 L 50 22.5 Z M 0 37.5 L 0 42.5 L 50 42.5 L 50 37.5 Z "/>
+                    </g>
+                </svg>
+            </div>
+
+            <ul className="main_nav">
+                <li onClick={ toogle_menu }><NavLink to="/acerca" className="link">Acerca</NavLink></li>
+                <li onClick={ toogle_menu }><NavLink to="/portafolio" className="link">Portafolio</NavLink></li>
+                <li onClick={ toogle_menu }><NavLink to="/trayectoria" className="link">Trayectoria</NavLink></li>
+                <li onClick={ toogle_menu }><NavLink to="/demos" className="link">Demo Reel</NavLink></li>
+                <li onClick={ toogle_menu }><NavLink to="/contacto" className="link">Contacto</NavLink></li>
+            </ul>
+
+        </React.Fragment>
     )
 }
 
