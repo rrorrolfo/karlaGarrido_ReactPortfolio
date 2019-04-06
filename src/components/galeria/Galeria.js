58ed9924a2dc2e02ff9b1ba6d3324bcec97b399e
Rodolfo Perez <rodolfobeste@gmail.com>
2019-04-06 14:49:01 +0300

    Styled galeria component for tablet and desktop screens

--- src/components/galeria/Galeria.js
+++ src/components/galeria/Galeria.js
@@ -1,10 +1,47 @@
 import React from "react";
-import img from "../../img/galeriaImgs/mexicana.jpg";
+import img from "../../img/galeriaImgs/18hermana.jpg";
+import suelo from "../../img/galeriaImgs/1suelo.jpg";
+import triciclo from "../../img/galeriaImgs/16triciclo.jpg";
 
 const Galeria = () => {
     return(
-        <div>
-           <img src={ img } alt="Mexicana" title="Mexicana"/>
+        <div className="Galeria">
+
+            <h1>Galeria de fotos</h1>
+
+            <div className="gallery_container">
+                <div className="photo-container">
+                    <div className="foto">
+                    <img src={ img } alt="Foto de galeria"/>
+                        <div className="sobre-foto"> 
+                        <h3>Mucho ruido y pocas nueces de William Shakespeare</h3>
+                        <p>Dirección: Juan Mora (2011)<br/>Teatro: Julio Castillo</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="photo-container">
+                    <div class="foto">
+                    <img src={ suelo } alt="Foto de galeria"/>
+                        <div class="sobre-foto"> 
+                        <h3>Mucho ruido y pocas nueces de William Shakespeare</h3>
+                        <p>Dirección: Juan Mora (2011)<br/>Teatro: Julio Castillo</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="photo-container">
+                    <div class="foto">
+                    <img src={ triciclo } alt="Foto de galeria"/>
+                        <div class="sobre-foto"> 
+                        <h3>Mucho ruido y pocas nueces de William Shakespeare</h3>
+                        <p>Dirección: Juan Mora (2011)<br/>Teatro: Julio Castillo</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            
         </div>
     )
 }
