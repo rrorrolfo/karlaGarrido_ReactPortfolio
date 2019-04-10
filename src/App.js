c4486669d76fc6fd9d2573fd153ade561a95ec20
Rodolfo Perez <rodolfobeste@gmail.com>
2019-04-10 16:04:34 +0300

    Removed Context

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import {
   Route,
   Redirect,
   Switch } from "react-router-dom";
-import { Provider } from "./components/context/index";
 import Header from "./components/Header";
 import Jumbotron from "./components/Jumbotron";
 import Acerca from "./components/acerca/Acerca";
@@ -16,49 +15,32 @@ import Galeria from "./components/galeria/Galeria";
 import Contacto from "./components/contacto/Contacto";
 import Footer from "./components/Footer";
 import SocialMedia from "./components/SocialMedia";
-import Loader from "./components/loader/Loader";
 import NotFound from "./components/NotFound";
 
 class App extends Component {
 
-  state = {
-    isLoading: true
-  }
-
-  toogleLoading = () => {
-    this.setState({
-      isLoading: !this.state.isLoading
-    })
-  }
-
   render() {
-    
     return (
 
-      <Provider value={ {
-        actions: {
-          toogleLoading: this.toogleLoading
-        }
-      } }>
-        <BrowserRouter>
-          <div>
-            <Header />
-            <Switch>
-              <Route exact path="/" component={ Jumbotron }/>
-              <Route path="/acerca" component={ Acerca }/>
-              <Route path="/portafolio" component={ Portafolio }/>
-              <Route path="/trayectoria" component={ Trayectoria }/>
-              <Route path="/demos" component={ Demos }/>
-              <Route path="/galeria" component={ Galeria }/>
-              <Route path="/contacto" component={ Contacto }/>
-              <Route path="/notfound" component={ NotFound }/>
-              <Route render={ () => <Redirect to="/notfound" /> }/>
-            </Switch>
-            <Footer />
-            <SocialMedia />
-          </div>
-        </BrowserRouter>
-      </Provider>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={ Jumbotron }/>
+            <Route path="/acerca" component={ Acerca }/>
+            <Route path="/portafolio" component={ Portafolio }/>
+            <Route path="/trayectoria" component={ Trayectoria }/>
+            <Route path="/demos" component={ Demos }/>
+            <Route path="/galeria" component={ Galeria }/>
+            <Route path="/contacto" component={ Contacto }/>
+            <Route path="/notfound" component={ NotFound }/>
+            <Route render={ () => <Redirect to="/notfound" /> }/>
+          </Switch>
+          <Footer />
+          <SocialMedia />
+        </div>
+      </BrowserRouter>
+
     );
   }
 }
--- src/components/context/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const LoadingContext = React.createContext();
-
-export const Provider = LoadingContext.Provider;
-export const Consumer = LoadingContext.Consumer;
