dac871c4d421270161e9ac45e1bdc2bc24585d8a
Rodolfo Perez <rodolfobeste@gmail.com>
2019-04-10 12:35:12 +0300

    Created and styled loader

---
+++ src/components/loader/Loader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./loader.css";
+
+const Loader = () => {
+    return(
+        <div className="sticky_footer">
+            <div className="loader">Loading...</div>
+        </div>
+    )
+}
+
+export default Loader;
