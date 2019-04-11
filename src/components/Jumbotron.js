972153a51d0ae2882befd3845a85f89a29d6b5ad
Rodolfo Perez <rodolfobeste@gmail.com>
2019-04-11 11:28:28 +0300

    Removed state

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Loader from "./loader/Loader";
 const portada_lg = require("../img/portada.jpg");
 const portada_mediana = require("../img/medium_portada.jpg");
 const portada_md = require("../img/mobile_portada2.jpg");
@@ -7,25 +6,9 @@ const portada_sm = require("../img/mobile_portada3.jpg");
 
 class Jumbotron extends Component  {
 
-    state = {
-        isLoading: true
-      }
-    
-    toogleLoading = () => {
-        this.setState({
-          isLoading: !this.state.isLoading
-        })
-      }
-
-    componentDidMount() {
-        this.toogleLoading();
-    }
-
     render() {
         return (
 
-            this.state.isLoading ?( <Loader />
-                ) : (
                 <div id="jumbotron">
                     <img src={ portada_sm } className="portada_sm" alt="Actriz Mexicana Karla Garrido" title="Actriz Mexicana Karla Garrido"/>
                     <img src={ portada_md } className="portada_md" alt="Actriz Mexicana Karla Garrido" title="Actriz Mexicana Karla Garrido"/>
@@ -37,7 +20,7 @@ class Jumbotron extends Component  {
                         <p>Actriz</p>
                     </div>
                 </div>
-            )
+            
         )
     }
 }
