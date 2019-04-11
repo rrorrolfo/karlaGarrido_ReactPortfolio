9785aadfc7656ea9a39b1800a813ad9ecaa032a2
Rodolfo Perez <rodolfobeste@gmail.com>
2019-04-11 11:29:37 +0300

    Added Loader display according to isLoading state

--- src/components/demos/Demos.js
+++ src/components/demos/Demos.js
@@ -1,33 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
+import Loader from "../loader/Loader";
 
-const Demos = () => {
-    return(
+class Demos extends Component {
 
-        <div id="demos" className="sticky_footer">
+    state = {
+        isLoading: true
+      }
+    
+    toogleLoading = () => {
+        this.setState({
+          isLoading: !this.state.isLoading
+        })
+      }
 
-            <div>
-                <iframe src="https://www.youtube.com/embed/OWk1NIT3PuY" title="Demo Reel 2018"></iframe>
-                <h2>Demo Reel 2018</h2>
-            </div>
+    componentDidMount() {
 
-            <div>
-                <iframe src="https://www.youtube.com/embed/2NQSHHJWxD0" title="Demo Reel 2016"></iframe>
-                <h2>Demo Reel 2016</h2>
-            </div>
+        setTimeout(this.toogleLoading, 4000)
+    
+    }
 
-            <div>
-                <iframe src="https://www.youtube.com/embed/9wVIiiLDv34" title="Demo Reel 2014"></iframe>
-                <h2>Demo Reel 2014</h2>
-            </div>
-            
-            <div>
-                <iframe src="https://www.youtube.com/embed/SxGLRXrPgaI" title="Demo Reel Comedia"></iframe>
-                <h2>Demo Reel "Comedia"</h2>
-            </div>
-
-        </div>
-
-    )
+    render() {
+        return(
+            <React.Fragment>
+                <Loader isLoading={ this.state.isLoading }/>
+                <div id="demos" className="sticky_footer">
+        
+                    <div>
+                        <iframe src="https://www.youtube.com/embed/OWk1NIT3PuY" title="Demo Reel 2018"></iframe>
+                        <h2>Demo Reel 2018</h2>
+                    </div>
+        
+                    <div>
+                        <iframe src="https://www.youtube.com/embed/2NQSHHJWxD0" title="Demo Reel 2016"></iframe>
+                        <h2>Demo Reel 2016</h2>
+                    </div>
+        
+                    <div>
+                        <iframe src="https://www.youtube.com/embed/9wVIiiLDv34" title="Demo Reel 2014"></iframe>
+                        <h2>Demo Reel 2014</h2>
+                    </div>
+                    
+                    <div>
+                        <iframe src="https://www.youtube.com/embed/SxGLRXrPgaI" title="Demo Reel Comedia"></iframe>
+                        <h2>Demo Reel "Comedia"</h2>
+                    </div>
+        
+                </div>
+            </React.Fragment>
+        )
+    }
+    
 }
 
 export default Demos;
